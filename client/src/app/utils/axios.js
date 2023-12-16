f32e2b73ae5e45fbd18a3ab2408fda237e583138
tuanht2k2 <tuanht2k2@gmail.com>
2023-12-16 12:06:00 +0700

    api config

--- client/src/app/utils/axios.js
+++ client/src/app/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const instance = axios.create({
-  baseURL: "http://localhost:8080/api",
+  baseURL: "https://k-editor-service.onrender.com/api",
   // timeout: 5000,
   headers: { "X-Custom-Header": "foobar" },
 });
